478a2294c1a0a7c80812755a8a7e8c1650156e40
karenmrdn <karenmrdn47@gmail.com>
2021-10-18 13:27:39 +0300

    Basic class created

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+}
+
+const accounting = new Department("Accounting");
+accounting.describe();
+
+const accountingCopy = { describe: accounting.describe };
+// accountingCopy.describe(); // error in TS with this of type Department in the describe method
