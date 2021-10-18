741e15c73b88a1cb43b433c29854bde0fa0d886c
karenmrdn <karenmrdn47@gmail.com>
2021-10-18 14:09:21 +0300

    Constructor refactored and some properties are private readonly now

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,35 @@
 class Department {
-  name: string;
+  //   private id: string;
+  //   private name: string;
+  private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: string, private name: string) {
+    // this.id = id;
+    // this.name = name;
   }
 
   describe(this: Department) {
-    console.log("Department: " + this.name);
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  showEmployeeInfo() {
+    console.log("Number of employees: " + this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("Accounting");
-accounting.describe();
+const accounting = new Department("d1", "Accounting");
 
-const accountingCopy = { describe: accounting.describe };
+accounting.addEmployee("Lara");
+accounting.addEmployee("Andrew");
+// accounting.employee[4] = "Karen"; // error in TS if field is private
+
+accounting.describe();
+accounting.showEmployeeInfo();
+
+// const accountingCopy = { describe: accounting.describe };
 // accountingCopy.describe(); // error in TS with this of type Department in the describe method
